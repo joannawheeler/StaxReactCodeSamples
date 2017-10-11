7bec793141e856e3791013cc85d8c86f2483253a
jtknox91 <jtknox91@gmail.com>
2017-10-10 22:46:16 -0700

    Configure container and authentication for cloudkit. Add seperate key file for .gitignore

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import './Auth.css';
 
+//CloudKit
+import CK from '../cloudkit/ck-connect.js';
+
 class Auth extends Component {
   constructor (props) {
     super(props);
 
   }
 
+  componentDidMount () {
+    CK.authenticate();
+  }
+
+  componentDidUpdate () {
+    CK.authenticate();
+  }
+
   render () {
     return (<div className="Auth">
       <div id="apple-sign-in-button"></div>
+      <div id="apple-sign-out-button"></div>
     </div>);
   }
 }
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import './Main.css';
 
+//CloudKit
+import CK from '../cloudkit/ck-connect.js';
+
 class Main extends Component {
   constructor (props) {
     super(props);
 
   }
 
+  componentDidMount () {
+    CK.authenticate();
+  }
+
+  componentDidUpdate () {
+    CK.authenticate();   
+  }
+
   render () {
     return (<div className="Main">
+      <div id="apple-sign-in-button"></div>
       <div id="apple-sign-out-button"></div>
     </div>);
   }
