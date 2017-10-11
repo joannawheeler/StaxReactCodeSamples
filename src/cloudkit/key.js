1da53dc29ac3e25f237f87e11294eeaf0f81cf49
jtknox91 <jtknox91@gmail.com>
2017-10-10 23:23:40 -0700

    Add example key file

---
+++ src/cloudkit/key.js
@@ -0,0 +1,7 @@
+export default "";
+/**
+  Save this file as `key.js` in the src/cloudkit directory.
+  The exported string should contain the API key for the cloudkit container. 
+  Having this file in .gitignore DOES NOT prevent the end user from seeing this key,
+  but it does keep the key out of our github repo during development.
+*/
